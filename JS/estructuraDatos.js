72357abca777bba3c19eeee5b7d43c7f939b57d8
Arturodev99 <arturo.grand1999@gmail.com>
2023-03-09 13:56:48 +0100

    estrucutura

---
+++ JS/estructuraDatos.js
@@ -0,0 +1,9 @@
+var niveles={'Personajes':[{nombre:'Jesus', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''},
+{nombre:'Juanma', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''},
+{nombre:'Rana', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''}],
+'escenarios':[{nombre:'',imagen:'',size:''},
+{nombre:'',imagen:'',size:''},
+{nombre:'',imagen:'',size:''},], 
+'enemigos':[{nombre:'' ,imagen:'',posicionX:'',posicionY:'',size:''},
+{nombre:'' ,imagen:'',posicionX:'',posicionY:'',size:''},
+{nombre:'' ,imagen:'',posicionX:'',posicionY:'',size:''},]}
