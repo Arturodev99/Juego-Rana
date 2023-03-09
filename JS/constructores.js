1bdbec3e5ae9e7936aaf1282437b3221bb3cbafc
Sergio Molina Albacete <sergio11albacete@gmail.com>
2023-03-09 14:03:50 +0100

    constructores

--- JS/constructores.js
+++ JS/constructores.js
@@ -1,5 +1,19 @@
 function Rana() {
     this.url = "direccionRana";
     this.vida = 3;
-    
-  }
+
+}
+
+function Fondo() {
+    this.urlFondo = "direccionImagenFondo";
+    this.numeroEnemigos = 
+}
+
+function Nivel() {
+    this.Fondo = new Fondo;
+    Rana = new Rana();
+}
+
+function Enemigo() {
+    this.urlFondo = "direccionImagenFondo";
+}
