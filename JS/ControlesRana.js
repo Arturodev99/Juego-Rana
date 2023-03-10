382b7557b73c383232a6f9b84671c9ca2dece055
Arturodev99 <arturo.grand1999@gmail.com>
2023-03-10 11:07:47 +0100

    controles

--- JS/ControlesRana.js
+++ JS/ControlesRana.js
@@ -1,15 +1,11 @@
-var PosicionOriginal=3;
+var PosicionOriginal=6;
         var PosicionNueva
         function Comienzo(){
             for(var i = 1; i<97;i++){
                 document.getElementById('pantalla').innerHTML+='<div id="'+i+'"class="casilla"></div>'
 
             }
-<<<<<<< Updated upstream
-            document.getElementById('6').style.backgroundImage='url('+niveles.Personajes[2].imagenParada+')';
-=======
-            document.getElementById('4').style.backgroundImage='url('+niveles.Personajes[2].imagenMovimiento;
->>>>>>> Stashed changes
+            document.getElementById('6').style.backgroundImage='url('+niveles.Personajes[2].imagenMovimiento;
             document.onkeyup=Teclado
 
         }
@@ -32,13 +28,10 @@ var PosicionOriginal=3;
                 else{
                     PosicionNueva=PosicionOriginal+1
                     document.getElementById(PosicionNueva).style.backgroundImage='url('+niveles.Personajes[2].imagenMovimiento+')';
-<<<<<<< Updated upstream
                     document.getElementById(PosicionOriginal).style.backgroundImage='';
                     document.getElementById(PosicionNueva).style.zIndex='1'
-=======
                     document.getElementById(PosicionNueva).style.backgroundSize='cover';
                     document.getElementById(PosicionOriginal).style.backgroundColor='inherit';
->>>>>>> Stashed changes
                     PosicionOriginal=PosicionNueva
                 }
                 
@@ -46,34 +39,25 @@ var PosicionOriginal=3;
             else if(tecla.keyCode == 32){
                 PosicionNueva=PosicionOriginal+12
                 document.getElementById(PosicionNueva).style.backgroundImage='url('+niveles.Personajes[2].imagenMovimiento+')';
-<<<<<<< Updated upstream
                 document.getElementById(PosicionOriginal).style.backgroundImage='';
-=======
                 document.getElementById(PosicionNueva).style.backgroundSize='cover';
                 document.getElementById(PosicionOriginal).style.backgroundColor='inherit';
->>>>>>> Stashed changes
                 PosicionOriginal=PosicionNueva
             }
             else if(tecla.keyCode == 83){
                 PosicionNueva=PosicionOriginal+12
                 document.getElementById(PosicionNueva).style.backgroundImage='url('+niveles.Personajes[2].imagenMovimiento+')';
-<<<<<<< Updated upstream
                 document.getElementById(PosicionOriginal).style.backgroundImage='';
-=======
                 document.getElementById(PosicionNueva).style.backgroundSize='cover';
                 document.getElementById(PosicionOriginal).style.backgroundImage='url('+niveles.Personajes[2].imagenParada+')';
->>>>>>> Stashed changes
                 PosicionOriginal=PosicionNueva
             }
             else if(tecla.keyCode == 87){
                 PosicionNueva=PosicionOriginal-12
                 document.getElementById(PosicionNueva).style.backgroundImage='url('+niveles.Personajes[2].imagenMovimiento+')';
-<<<<<<< Updated upstream
                 document.getElementById(PosicionOriginal).style.backgroundImage='';
-=======
                 document.getElementById(PosicionNueva).style.backgroundSize='cover';
                 document.getElementById(PosicionOriginal).style.backgroundColor='inherit';
->>>>>>> Stashed changes
                 PosicionOriginal=PosicionNueva
             }
             
