e4b5582bf721aad02897a32af88731a64771c2d8
Arturodev99 <arturo.grand1999@gmail.com>
2023-03-10 08:43:06 +0100

    Estructura de datos finalizada

--- JS/estructuraDatos.js
+++ JS/estructuraDatos.js
@@ -1,9 +1,11 @@
-var niveles={'Personajes':[{nombre:'Jesus', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''},
+var niveles={
+'Personajes':[{nombre:'Jesus', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''},
 {nombre:'Juanma', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''},
 {nombre:'Rana', imagenParada:'../Media/Personajes/Rana.png', imagenMovimiento:'../Media/Personajes/RanaMovimineto.gif', posicionX:'550', posicionY:'0'}],
-'escenarios':[{nombre:'',imagen:'',size:''},
-{nombre:'',imagen:'',size:''},
-{nombre:'',imagen:'',size:''},], 
+'escenarios':[{nombre:'1',imagen:'../Media/NivelesTerminados/MapaArboles.jpg',sizeX:'800', sizeY:'1200'},
+{nombre:'2',imagen:'../Media/NivelesTerminados/NivelVolcanico.jpg',sizeX:'800', sizeY:'1200'},
+{nombre:'3',imagen:'../Media/NivelesTerminados/NivelPirata.jpg',sizeX:'800', sizeY:'1200'},], 
 'enemigos':[{nombre:'' ,imagen:'',posicionX:'',posicionY:'',size:''},
 {nombre:'' ,imagen:'',posicionX:'',posicionY:'',size:''},
-{nombre:'' ,imagen:'',posicionX:'',posicionY:'',size:''},]}
+{nombre:'' ,imagen:'',posicionX:'',posicionY:'',size:''},]
+}
