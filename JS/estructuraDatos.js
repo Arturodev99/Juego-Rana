600253373fbc820aba856a1937386e10a56d85ac
Arturodev99 <arturo.grand1999@gmail.com>
2023-03-10 08:39:47 +0100

    Estructura de datos

--- JS/estructuraDatos.js
+++ JS/estructuraDatos.js
@@ -1,6 +1,6 @@
 var niveles={'Personajes':[{nombre:'Jesus', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''},
 {nombre:'Juanma', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''},
-{nombre:'Rana', imagenParada:'', imagenMovimiento:'', posicionX:'', posicionY:''}],
+{nombre:'Rana', imagenParada:'../Media/Personajes/Rana.png', imagenMovimiento:'../Media/Personajes/RanaMovimineto.gif', posicionX:'550', posicionY:'0'}],
 'escenarios':[{nombre:'',imagen:'',size:''},
 {nombre:'',imagen:'',size:''},
 {nombre:'',imagen:'',size:''},], 
