a0621de032f72ecfd93dc1ff7a68b87212f649fd
Sergio Molina Albacete <sergio11albacete@gmail.com>
2023-03-10 10:53:25 +0100

    RanaCreadaEnSuSitio

--- JS/constructores.js
+++ JS/constructores.js
@@ -1,7 +1,20 @@
 function Personaje() {
-    this.url = "direcMedia/Images/Personajes/Rana.png";
     this.vida = 3;
+    this.x = window.innerWidth / 2 - 100;
+    this.y = 20;
+    this.contenedor = document.createElement("div");
+    this.contenedor.style.left = "50%";
+    this.contenedor.style.left = this.x + "px";
+    this.contenedor.style.top = this.y + "px";
+    this.img = document.createElement("img");
+    this.img.src = "../Media/Images/Personajes/Rana.png";
+    this.img.style.width = "100px"
+    this.contenedor.appendChild(this.img);
+    document.getElementById("contenedor-personajes").appendChild(this.contenedor);
 }
+
+
+
 
 function Fondo() {
     this.urlFondo = "direccionImagenFondo";
@@ -19,3 +32,4 @@ function Enemigo() {
     this.posicionY = 0
     this.velocidad = 0
 }
+
