8045d54f05aaa4fd3b0e587812e1184241b6d3ec
Sergio Molina Albacete <sergio11albacete@gmail.com>
2023-03-23 08:58:35 +0100

    pruebaParaTocarConstructores

--- JS/constructores.js
+++ JS/constructores.js
@@ -1,36 +1,24 @@
 //Terminado constructor personaje
 function Personaje(eleccion) {
+    this.vida = 3;
+    this.x = window.innerWidth / 2 - 100;
+    this.y = 5;
+    this.contenedor = document.createElement("div");
+    this.contenedor.style.position = "absolute";
+    this.contenedor.style.left = "50%";
+    this.contenedor.style.transform = "translate(-50%, 0)";
+    this.contenedor.style.top = this.y + "px";
+    this.img = document.createElement("img");
+    this.img.src = "../Media/Images/Personajes/" + eleccion;
+    this.img.style.width = "80px";
+    this.contenedor.appendChild(this.img);
+    document.querySelector("body").appendChild(this.contenedor);
     if (eleccion == "Rana.png") {
-        this.vida = 3;
-        this.x = window.innerWidth / 2 - 100;
-        this.y = 5;
-        this.contenedor = document.createElement("div");
-        this.contenedor.style.position = "absolute";
-        this.contenedor.style.left = "50%";
-        this.contenedor.style.transform = "translate(-50%, 0)";
-        this.contenedor.style.top = this.y + "px";
-        this.img = document.createElement("img");
-        this.img.src = "../Media/Images/Personajes/" + eleccion;
         this.img.style.width = "80px";
-        this.contenedor.appendChild(this.img);
-        document.querySelector("body").appendChild(this.contenedor);
     }
-
     if (eleccion == "Juanma.png" || eleccion == "Jesus.png") {
-        this.vida = 3;
-        this.x = window.innerWidth / 2 - 100;
-        this.y = 5;
-        this.contenedor = document.createElement("div");
-        this.contenedor.style.position = "absolute";
-        this.contenedor.style.left = "50%";
-        this.contenedor.style.transform = "translate(-50%, 0)";
-        this.contenedor.style.top = this.y + "px";
-        this.img = document.createElement("img");
-        this.img.src = "../Media/Images/Personajes/" + eleccion;
         this.img.style.width = "50px";
         this.img.style.height = "80px";
-        this.contenedor.appendChild(this.img);
-        document.querySelector("body").appendChild(this.contenedor);
     }
 }
 
