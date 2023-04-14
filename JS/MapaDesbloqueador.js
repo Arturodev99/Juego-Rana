83e7d49c9110edf40fd8cd414d4ae266ec68c4f5
Sergio Molina Albacete <sergio11albacete@gmail.com>
2023-04-14 12:33:33 +0200

    cambios

--- JS/MapaDesbloqueador.js
+++ JS/MapaDesbloqueador.js
@@ -1,6 +1,27 @@
 function desbloquearMapa(){
 
-    if(d){
+    if(mapas[0] == true){
+        document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla2'></div></a>"
+        document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2.png')"
+    }
 
+    if(mapas[1] == true){
+        document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla3'></div></a>"
+        document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2+3.png')"
+    }
+
+    if(mapas[2] == true){
+        document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla4'></div></a>"
+        document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2+3+4.png')"
+    }
+
+    if(mapas[3] == true){
+        document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla5'></div></a>"
+        document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2+3+4+5.png')"
+    }
+
+    if(mapas[4] == true){
+        document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla6'></div></a>"
+        document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanitaCompletado.png')"
     }
 }
