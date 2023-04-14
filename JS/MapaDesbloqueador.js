03223f1faba274139cd1ae66b2614605c871356c
Arturodev99 <arturo.grand1999@gmail.com>
2023-04-14 13:46:06 +0200

    controls

--- JS/MapaDesbloqueador.js
+++ JS/MapaDesbloqueador.js
@@ -1,26 +1,26 @@
 function desbloquearMapa(){
 
-    if(mapas[0] == true){
+    if(localStorage.getItem('mundoActual')[0] == true){
         document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla2'></div></a>"
         document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2.png')"
     }
 
-    if(mapas[1] == true){
+    if(localStorage.getItem('mundoActual')[1] == true){
         document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla3'></div></a>"
         document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2+3.png')"
     }
 
-    if(mapas[2] == true){
+    if(localStorage.getItem('mundoActual')[2] == true){
         document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla4'></div></a>"
         document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2+3+4.png')"
     }
 
-    if(mapas[3] == true){
+    if(localStorage.getItem('mundoActual')[3] == true){
         document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla5'></div></a>"
         document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanita1+2+3+4+5.png')"
     }
 
-    if(mapas[4] == true){
+    if(localStorage.getItem('mundoActual')[5] == true){
         document.getElementById("contenedorMapas").innerHTML += "<a href='nivel0.html'><div id='isla6'></div></a>"
         document.getElementById("contenedorMapas").style.backgroundImage = "url('Media/Images/ImgSeleccionMundo/MapaRanitaCompletado.png')"
     }
