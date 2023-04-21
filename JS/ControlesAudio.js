b14bf897e17d34cbac9a1c554569eda89d5c42c1
17manub <127190604+17manub@users.noreply.github.com>
2023-04-21 09:52:18 +0200

    musicaa

--- JS/ControlesAudio.js
+++ JS/ControlesAudio.js
@@ -1,4 +1,5 @@
 var VentanaAbierta= false
+var Spotify
 function ActivarPanelAudio(){
     if (VentanaAbierta == false){
         document.getElementById('VentanadeControlSonido').style.display='block';
@@ -15,11 +16,33 @@ function ActivarMusica(respuesta){
         case 'si':
             document.getElementById('MusicaSi').src="../Media/Images/marco_musica.png"
             document.getElementById('MusicaNo').src="../Media/Images/marco_personaje_hover.png"
-            
+            ReproducirMusica()
             break;
         case 'no':
             document.getElementById('MusicaNo').src="../Media/Images/marco_musica.png"
             document.getElementById('MusicaSi').src="../Media/Images/marco_personaje_hover.png"
             break;
     }
+}
+function ReproducirMusica(){
+    Spotify=document.getElementById('Reproductor')
+    nivel = parseInt(localStorage.getItem('nivelActual'));
+    alert(nivel)
+    if(nivel <= 4){
+        Spotify.src='../Media/musica/kazan_lr.n.32.mp3';
+    }
+    else if(nivel <= 9){
+        Spotify.src='../Media/musica/Desert Hills - Mario Kart DS Music Extended.mp3';
+    }
+    else if(nivel <= 14){
+        Spotify.src='../Media/musica/x2mate.com - Jungle Race_ Remaster ► Lego Stunt Rally (128 kbps).mp3';
+    }
+    else if(nivel <= 19){
+        alert('cielo')
+    }
+    else if(nivel <= 24){
+        Spotify.src='../Media/musica/Sonic Mania OST - Studiopolis Act 1.mp3';
+    }
+    Spotify.play()
+
 }
