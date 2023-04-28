d5427a37d7550a951d936cd5ae67412ca62ae0f2
Sergio Molina Albacete <sergio11albacete@gmail.com>
2023-04-28 08:58:16 +0200

    personajes se desbloquean correctamente

--- JS/activarPersonajes.js
+++ JS/activarPersonajes.js
@@ -3,20 +3,43 @@ function activarPersonaje() {
      
  
      if (mapas >= 5) {
-         document.getElementById('contenedorLink').onclick='personajeElegido("3");';
+        document.getElementById('contenedorLink').addEventListener('click', function() {
+            personajeElegido('3');
+          });
+          
          document.getElementById('personajeLink').src = '../Media/Images/Personajes/link/link_quieto.png'
      }
  
-     if (mapas >= 10) {
-     }
+    //  if (mapas >= 10) {
+    //     document.getElementById('').addEventListener('click', function() {
+    //         personajeElegido("");
+    //       });
+          
+    //      document.getElementById('').src = '../Media/Images/Personajes/link/link_quieto.png'
+    //  }
  
-     if (mapas >= 15) {
-     }
+    //  if (mapas >= 15) {
+    //     document.getElementById('').addEventListener('click', function() {
+    //         personajeElegido("");
+    //       });
+          
+    //      document.getElementById('').src = '../Media/Images/Personajes/link/link_quieto.png'
+    //  }
  
-     if (mapas >= 20) {
-     }
+      if (mapas >= 20) {
+         document.getElementById('contenedorJesus').addEventListener('click', function() {
+             personajeElegido("2");
+          });
+          
+          document.getElementById('personajeJesus').src = '../Media/Images/Personajes/jesus.png'
+      }
  
      if (mapas >= 25) {
+        document.getElementById('contenedorJuanma').addEventListener('click', function() {
+            personajeElegido("1");
+          });
+          
+         document.getElementById('personajeJuanma').src = '../Media/Images/Personajes/juanma.png'
      }
 
 }
