24e4a93881ad3f6a97d41006d4e5554252c3345f
17manub <127190604+17manub@users.noreply.github.com>
2023-05-04 08:38:23 +0200

    aaaa

--- JS/ControlesAudio.js
+++ JS/ControlesAudio.js
@@ -32,18 +32,21 @@ function ReproducirMusica(){
     nivel = parseInt(localStorage.getItem('nivelActual'));
     //alert(nivel)
     if(nivel <= 4){
-        Spotify.src='../Media/musica/kazan_lr.n.32.mp3';
+        Spotify.src='../Media/musica/x2mate.com - Jungle Race_ Remaster ► Lego Stunt Rally (128 kbps).mp3';
     }
     else if(nivel <= 9){
         Spotify.src='../Media/musica/Desert Hills - Mario Kart DS Music Extended.mp3';
     }
     else if(nivel <= 14){
-        Spotify.src='../Media/musica/x2mate.com - Jungle Race_ Remaster ► Lego Stunt Rally (128 kbps).mp3';
+        Spotify.src='../Media/musica/Gritzy Caves Underground Theme [Mario & Luigi Partners in Time].mp3';
     }
     else if(nivel <= 19){
-        Spotify.src='../Media/musica/Bianco Hills - Super Mario Sunshine.mp3';
+        Spotify.src='../Media/musica/kazan_lr.n.32.mp3';
     }
     else if(nivel <= 24){
+        Spotify.src='../Media/musica/Bianco Hills - Super Mario Sunshine.mp3';
+    }
+    else if(nivel <= 29){
         Spotify.src='../Media/musica/Sonic Mania OST - Studiopolis Act 1.mp3';
     }
     Spotify.play()
